26aeb4239d2387545798cbabc52aaa957354498f
lam6670 <55042764+lam6670@users.noreply.github.com>
2021-11-04 21:14:49 +0700

    1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import HeroesPage from './heroes/HeroesPage.js';
+function App() {
+  return (
+    <div className="App">
+      <HeroesPage></HeroesPage>
+    </div>
+  );
+}
+
+export default App;
